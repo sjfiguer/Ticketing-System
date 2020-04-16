37c2e8cd0cae89f65c6e8bb30404ae4c9290cde7
acschutt <acschutt@ISYS4363-03>
2020-04-16 12:50:51 -0500

    Editing design on Admin Homepage

--- Ticketing System/Admin Homepage.Designer.cs
+++ Ticketing System/Admin Homepage.Designer.cs
@@ -39,50 +39,58 @@
             // 
             // Admin_Home_btn
             // 
+            this.Admin_Home_btn.BackColor = System.Drawing.Color.DarkRed;
             this.Admin_Home_btn.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.Admin_Home_btn.Location = new System.Drawing.Point(848, 609);
-            this.Admin_Home_btn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Admin_Home_btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.Admin_Home_btn.Location = new System.Drawing.Point(900, 641);
+            this.Admin_Home_btn.Margin = new System.Windows.Forms.Padding(6);
             this.Admin_Home_btn.Name = "Admin_Home_btn";
             this.Admin_Home_btn.Size = new System.Drawing.Size(264, 244);
             this.Admin_Home_btn.TabIndex = 7;
             this.Admin_Home_btn.Text = "Queue";
-            this.Admin_Home_btn.UseVisualStyleBackColor = true;
+            this.Admin_Home_btn.UseVisualStyleBackColor = false;
             this.Admin_Home_btn.Click += new System.EventHandler(this.Admin_Home_btn_Click);
             // 
             // Class_Erollment_btn
             // 
+            this.Class_Erollment_btn.BackColor = System.Drawing.Color.DarkRed;
             this.Class_Erollment_btn.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.Class_Erollment_btn.Location = new System.Drawing.Point(572, 609);
-            this.Class_Erollment_btn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Class_Erollment_btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.Class_Erollment_btn.Location = new System.Drawing.Point(624, 641);
+            this.Class_Erollment_btn.Margin = new System.Windows.Forms.Padding(6);
             this.Class_Erollment_btn.Name = "Class_Erollment_btn";
             this.Class_Erollment_btn.Size = new System.Drawing.Size(264, 244);
             this.Class_Erollment_btn.TabIndex = 6;
             this.Class_Erollment_btn.Text = "Student Info";
-            this.Class_Erollment_btn.UseVisualStyleBackColor = true;
+            this.Class_Erollment_btn.UseVisualStyleBackColor = false;
             this.Class_Erollment_btn.Click += new System.EventHandler(this.Class_Erollment_btn_Click);
             // 
             // Create_Account_btn
             // 
+            this.Create_Account_btn.BackColor = System.Drawing.Color.DarkRed;
             this.Create_Account_btn.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.Create_Account_btn.Location = new System.Drawing.Point(848, 353);
-            this.Create_Account_btn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Create_Account_btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.Create_Account_btn.Location = new System.Drawing.Point(900, 385);
+            this.Create_Account_btn.Margin = new System.Windows.Forms.Padding(6);
             this.Create_Account_btn.Name = "Create_Account_btn";
             this.Create_Account_btn.Size = new System.Drawing.Size(264, 244);
             this.Create_Account_btn.TabIndex = 5;
             this.Create_Account_btn.Text = "Create Account";
-            this.Create_Account_btn.UseVisualStyleBackColor = true;
+            this.Create_Account_btn.UseVisualStyleBackColor = false;
             this.Create_Account_btn.Click += new System.EventHandler(this.Create_Account_btn_Click);
             // 
             // Third_Party_btn
             // 
+            this.Third_Party_btn.BackColor = System.Drawing.Color.DarkRed;
             this.Third_Party_btn.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.Third_Party_btn.Location = new System.Drawing.Point(572, 353);
-            this.Third_Party_btn.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Third_Party_btn.ForeColor = System.Drawing.SystemColors.Control;
+            this.Third_Party_btn.Location = new System.Drawing.Point(624, 385);
+            this.Third_Party_btn.Margin = new System.Windows.Forms.Padding(6);
             this.Third_Party_btn.Name = "Third_Party_btn";
             this.Third_Party_btn.Size = new System.Drawing.Size(264, 244);
             this.Third_Party_btn.TabIndex = 4;
             this.Third_Party_btn.Text = "3rd Party Complaint";
-            this.Third_Party_btn.UseVisualStyleBackColor = true;
+            this.Third_Party_btn.UseVisualStyleBackColor = false;
             this.Third_Party_btn.Click += new System.EventHandler(this.Third_Party_btn_Click);
             // 
             // pictureBox1
@@ -108,6 +116,7 @@
             this.Controls.Add(this.Third_Party_btn);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Admin_Homepage";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin_Homepage";
             this.Load += new System.EventHandler(this.Admin_Homepage_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
