c8f03951d4a049ccf7d124bfd08f12431e70dc7f
lmw030 <lmw030@ISYS4363-02>
2020-04-29 12:04:20 -0500

    Fixed Submit Button

--- Ticketing System/Submit Ticket.Designer.cs
+++ Ticketing System/Submit Ticket.Designer.cs
@@ -61,9 +61,8 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, -2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1196, 172);
+            this.pictureBox1.Size = new System.Drawing.Size(897, 140);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
@@ -73,10 +72,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.label2.ForeColor = System.Drawing.Color.DarkRed;
-            this.label2.Location = new System.Drawing.Point(1092, 208);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(819, 169);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(66, 21);
+            this.label2.Size = new System.Drawing.Size(52, 16);
             this.label2.TabIndex = 17;
             this.label2.Text = "Logoff";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -86,10 +84,9 @@
             this.RegistrationLabel.AutoSize = true;
             this.RegistrationLabel.Font = new System.Drawing.Font("Mongolian Baiti", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.RegistrationLabel.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.RegistrationLabel.Location = new System.Drawing.Point(484, 208);
-            this.RegistrationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.RegistrationLabel.Location = new System.Drawing.Point(363, 169);
             this.RegistrationLabel.Name = "RegistrationLabel";
-            this.RegistrationLabel.Size = new System.Drawing.Size(354, 31);
+            this.RegistrationLabel.Size = new System.Drawing.Size(286, 25);
             this.RegistrationLabel.TabIndex = 15;
             this.RegistrationLabel.Text = "Walton IT Ticketing System";
             // 
@@ -98,10 +95,9 @@
             this.LastName.AutoSize = true;
             this.LastName.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.LastName.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.LastName.Location = new System.Drawing.Point(83, 324);
-            this.LastName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LastName.Location = new System.Drawing.Point(62, 263);
             this.LastName.Name = "LastName";
-            this.LastName.Size = new System.Drawing.Size(58, 21);
+            this.LastName.Size = new System.Drawing.Size(46, 16);
             this.LastName.TabIndex = 18;
             this.LastName.Text = "Name";
             // 
@@ -110,10 +106,9 @@
             this.Email.AutoSize = true;
             this.Email.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.Email.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.Email.Location = new System.Drawing.Point(825, 324);
-            this.Email.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Email.Location = new System.Drawing.Point(619, 263);
             this.Email.Name = "Email";
-            this.Email.Size = new System.Drawing.Size(59, 21);
+            this.Email.Size = new System.Drawing.Size(45, 16);
             this.Email.TabIndex = 19;
             this.Email.Text = "Email";
             // 
@@ -122,47 +117,42 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label5.Location = new System.Drawing.Point(85, 441);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(64, 358);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(346, 21);
+            this.label5.Size = new System.Drawing.Size(266, 16);
             this.label5.TabIndex = 20;
             this.label5.Text = "Please describe the issues you are facing:";
             // 
             // LastNametxt
             // 
             this.LastNametxt.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.LastNametxt.Location = new System.Drawing.Point(152, 319);
-            this.LastNametxt.Margin = new System.Windows.Forms.Padding(4);
+            this.LastNametxt.Location = new System.Drawing.Point(114, 259);
             this.LastNametxt.Name = "LastNametxt";
-            this.LastNametxt.Size = new System.Drawing.Size(361, 31);
+            this.LastNametxt.Size = new System.Drawing.Size(272, 26);
             this.LastNametxt.TabIndex = 22;
             // 
             // Emailtxt
             // 
             this.Emailtxt.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.Emailtxt.Location = new System.Drawing.Point(912, 319);
-            this.Emailtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.Emailtxt.Location = new System.Drawing.Point(684, 259);
             this.Emailtxt.Name = "Emailtxt";
-            this.Emailtxt.Size = new System.Drawing.Size(246, 31);
+            this.Emailtxt.Size = new System.Drawing.Size(186, 26);
             this.Emailtxt.TabIndex = 23;
             // 
             // DescribeTxt
             // 
-            this.DescribeTxt.Location = new System.Drawing.Point(89, 479);
-            this.DescribeTxt.Margin = new System.Windows.Forms.Padding(4);
+            this.DescribeTxt.Location = new System.Drawing.Point(67, 389);
             this.DescribeTxt.Name = "DescribeTxt";
-            this.DescribeTxt.Size = new System.Drawing.Size(1069, 117);
+            this.DescribeTxt.Size = new System.Drawing.Size(803, 96);
             this.DescribeTxt.TabIndex = 24;
             this.DescribeTxt.Text = "";
             // 
             // Submit_btn
             // 
             this.Submit_btn.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.Submit_btn.Location = new System.Drawing.Point(1032, 617);
-            this.Submit_btn.Margin = new System.Windows.Forms.Padding(4);
+            this.Submit_btn.Location = new System.Drawing.Point(774, 501);
             this.Submit_btn.Name = "Submit_btn";
-            this.Submit_btn.Size = new System.Drawing.Size(126, 46);
+            this.Submit_btn.Size = new System.Drawing.Size(94, 37);
             this.Submit_btn.TabIndex = 25;
             this.Submit_btn.Text = "Submit";
             this.Submit_btn.UseVisualStyleBackColor = true;
@@ -173,10 +163,9 @@
             this.PriorityCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.PriorityCB.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.PriorityCB.FormattingEnabled = true;
-            this.PriorityCB.Location = new System.Drawing.Point(912, 386);
-            this.PriorityCB.Margin = new System.Windows.Forms.Padding(4);
+            this.PriorityCB.Location = new System.Drawing.Point(684, 314);
             this.PriorityCB.Name = "PriorityCB";
-            this.PriorityCB.Size = new System.Drawing.Size(132, 29);
+            this.PriorityCB.Size = new System.Drawing.Size(100, 24);
             this.PriorityCB.TabIndex = 32;
             // 
             // CategoryCB
@@ -184,10 +173,9 @@
             this.CategoryCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.CategoryCB.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.CategoryCB.FormattingEnabled = true;
-            this.CategoryCB.Location = new System.Drawing.Point(584, 386);
-            this.CategoryCB.Margin = new System.Windows.Forms.Padding(4);
+            this.CategoryCB.Location = new System.Drawing.Point(438, 314);
             this.CategoryCB.Name = "CategoryCB";
-            this.CategoryCB.Size = new System.Drawing.Size(207, 29);
+            this.CategoryCB.Size = new System.Drawing.Size(156, 24);
             this.CategoryCB.TabIndex = 30;
             this.CategoryCB.SelectedIndexChanged += new System.EventHandler(this.CategoryCB_SelectedIndexChanged);
             // 
@@ -195,10 +183,9 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.label8.Location = new System.Drawing.Point(825, 390);
-            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Location = new System.Drawing.Point(619, 317);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(78, 21);
+            this.label8.Size = new System.Drawing.Size(59, 16);
             this.label8.TabIndex = 28;
             this.label8.Text = "Priority ";
             // 
@@ -206,43 +193,39 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.label9.Location = new System.Drawing.Point(464, 390);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(348, 317);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(84, 21);
+            this.label9.Size = new System.Drawing.Size(66, 16);
             this.label9.TabIndex = 27;
             this.label9.Text = "Category";
             // 
             // UserIDtxt
             // 
             this.UserIDtxt.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.UserIDtxt.Location = new System.Drawing.Point(636, 319);
-            this.UserIDtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.UserIDtxt.Location = new System.Drawing.Point(477, 259);
             this.UserIDtxt.Name = "UserIDtxt";
-            this.UserIDtxt.Size = new System.Drawing.Size(155, 31);
+            this.UserIDtxt.Size = new System.Drawing.Size(136, 26);
             this.UserIDtxt.TabIndex = 34;
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.label10.Location = new System.Drawing.Point(555, 322);
-            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Location = new System.Drawing.Point(395, 263);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(69, 21);
+            this.label10.Size = new System.Drawing.Size(76, 16);
             this.label10.TabIndex = 33;
-            this.label10.Text = "UserID";
+            this.label10.Text = "UserName";
             // 
             // TicketIDtxt
             // 
             this.TicketIDtxt.BackColor = System.Drawing.SystemColors.MenuBar;
             this.TicketIDtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.TicketIDtxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.TicketIDtxt.Location = new System.Drawing.Point(251, 251);
-            this.TicketIDtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.TicketIDtxt.Location = new System.Drawing.Point(188, 204);
             this.TicketIDtxt.Multiline = true;
             this.TicketIDtxt.Name = "TicketIDtxt";
-            this.TicketIDtxt.Size = new System.Drawing.Size(147, 27);
+            this.TicketIDtxt.Size = new System.Drawing.Size(110, 22);
             this.TicketIDtxt.TabIndex = 38;
             this.TicketIDtxt.TextChanged += new System.EventHandler(this.TicketIDtxt_TextChanged);
             // 
@@ -251,10 +234,9 @@
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.label11.ForeColor = System.Drawing.Color.DarkRed;
-            this.label11.Location = new System.Drawing.Point(83, 252);
-            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Location = new System.Drawing.Point(62, 205);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(154, 21);
+            this.label11.Size = new System.Drawing.Size(120, 16);
             this.label11.TabIndex = 37;
             this.label11.Text = "Your TicketID is:";
             // 
@@ -262,10 +244,9 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.linkLabel1.Location = new System.Drawing.Point(455, 619);
-            this.linkLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(341, 503);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(36, 17);
+            this.linkLabel1.Size = new System.Drawing.Size(28, 13);
             this.linkLabel1.TabIndex = 36;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "FAQ";
@@ -277,10 +258,9 @@
             this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox2.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.richTextBox2.Location = new System.Drawing.Point(89, 617);
-            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox2.Location = new System.Drawing.Point(67, 501);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(892, 45);
+            this.richTextBox2.Size = new System.Drawing.Size(669, 37);
             this.richTextBox2.TabIndex = 35;
             this.richTextBox2.Text = "Thank you contacting IT Support. Please check out our           page for further " +
     "information on any issues you are having until someone contacts you.";
@@ -289,10 +269,9 @@
             // 
             this.Department.AutoSize = true;
             this.Department.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
-            this.Department.Location = new System.Drawing.Point(85, 390);
-            this.Department.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Department.Location = new System.Drawing.Point(64, 317);
             this.Department.Name = "Department";
-            this.Department.Size = new System.Drawing.Size(94, 21);
+            this.Department.Size = new System.Drawing.Size(75, 16);
             this.Department.TabIndex = 40;
             this.Department.Text = "User Type";
             // 
@@ -301,10 +280,9 @@
             this.DepartmentCB.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.DepartmentCB.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.DepartmentCB.FormattingEnabled = true;
-            this.DepartmentCB.Location = new System.Drawing.Point(216, 386);
-            this.DepartmentCB.Margin = new System.Windows.Forms.Padding(4);
+            this.DepartmentCB.Location = new System.Drawing.Point(162, 314);
             this.DepartmentCB.Name = "DepartmentCB";
-            this.DepartmentCB.Size = new System.Drawing.Size(207, 29);
+            this.DepartmentCB.Size = new System.Drawing.Size(156, 24);
             this.DepartmentCB.TabIndex = 41;
             // 
             // Datetxt
@@ -312,12 +290,11 @@
             this.Datetxt.BackColor = System.Drawing.SystemColors.MenuBar;
             this.Datetxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.Datetxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.Datetxt.Location = new System.Drawing.Point(87, 208);
-            this.Datetxt.Margin = new System.Windows.Forms.Padding(4);
+            this.Datetxt.Location = new System.Drawing.Point(65, 169);
             this.Datetxt.Multiline = true;
             this.Datetxt.Name = "Datetxt";
             this.Datetxt.ReadOnly = true;
-            this.Datetxt.Size = new System.Drawing.Size(188, 41);
+            this.Datetxt.Size = new System.Drawing.Size(141, 33);
             this.Datetxt.TabIndex = 42;
             this.Datetxt.Text = "DATE";
             // 
@@ -327,11 +304,10 @@
             this.CATIDtxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.CATIDtxt.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.CATIDtxt.ForeColor = System.Drawing.Color.Transparent;
-            this.CATIDtxt.Location = new System.Drawing.Point(0, 673);
-            this.CATIDtxt.Margin = new System.Windows.Forms.Padding(4);
+            this.CATIDtxt.Location = new System.Drawing.Point(0, 547);
             this.CATIDtxt.Name = "CATIDtxt";
             this.CATIDtxt.ReadOnly = true;
-            this.CATIDtxt.Size = new System.Drawing.Size(133, 24);
+            this.CATIDtxt.Size = new System.Drawing.Size(100, 19);
             this.CATIDtxt.TabIndex = 43;
             this.CATIDtxt.Text = "CATID";
             // 
@@ -340,19 +316,18 @@
             this.LoginIDlbl.AutoSize = true;
             this.LoginIDlbl.Font = new System.Drawing.Font("Mongolian Baiti", 12F);
             this.LoginIDlbl.ForeColor = System.Drawing.Color.Transparent;
-            this.LoginIDlbl.Location = new System.Drawing.Point(357, 673);
-            this.LoginIDlbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LoginIDlbl.Location = new System.Drawing.Point(268, 547);
             this.LoginIDlbl.Name = "LoginIDlbl";
-            this.LoginIDlbl.Size = new System.Drawing.Size(79, 21);
+            this.LoginIDlbl.Size = new System.Drawing.Size(63, 16);
             this.LoginIDlbl.TabIndex = 45;
             this.LoginIDlbl.Text = "LoginID";
             // 
             // Submit_Ticket
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(1232, 738);
+            this.ClientSize = new System.Drawing.Size(924, 600);
             this.Controls.Add(this.LoginIDlbl);
             this.Controls.Add(this.CATIDtxt);
             this.Controls.Add(this.Datetxt);
@@ -378,7 +353,6 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.RegistrationLabel);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Submit_Ticket";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Submit_Ticket";
